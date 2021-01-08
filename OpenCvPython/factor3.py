30f50a7a43954bd2d2abe61e925944208c02cfdd
Arunava Bhattacharya <arunavabhattacharya89@gmail.com>
2021-01-08 23:04:29 +0530

    Commiting Demo App

---
+++ OpenCvPython/factor3.py
@@ -0,0 +1,10 @@
+import numpy as np
+# Input the Size of Array
+# creating an empty list
+a = []
+# number of elements as input
+n = int(input("Enter number of elements : "))
+# iterating till the range
+for i in range(0, n):
+    ele = int(input())
+    a.append(ele)  # adding the element
